74be60c5e1035eb8a4a2e4d3e3550ec7c3faca89
mac <mac@macs-MacBook-Pro.local>
2023-01-05 16:16:13 +0800

    android has no function pthread_cancel

--- programs/programs_helper.h
+++ programs/programs_helper.h
@@ -86,5 +86,9 @@ debug_set_target(FILE *fp);
 
 #define FUZZER_ASSERT(x) if (!(x)) { printf("USRSCTP assertion failed: function %s, file %s, line %d.\n", __PRETTY_FUNCTION__, __FILE__, __LINE__); abort(); }
  
+#define SIG_CANCEL_SIGNAL SIGUSR1
+static inline int pthread_cancel(pthread_t thread) {
+	return pthread_kill(thread, SIG_CANCEL_SIGNAL);
+}
 
 #endif /* __PROGRAMS_HELPER_H__ */
